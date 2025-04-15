b21353e68844cec58c82eba82a3415e9c84e05a4
mahdiengr <mahdi.engr@gmail.com>
2025-04-15 17:00:23 +0600

    Fibonaci function after edit added

--- Fibonaci.py
+++ Fibonaci.py
@@ -1,10 +1,24 @@
 
 def fibonaci(x,y):
-    for i in range(100):
-        z=x+y
-        y=x
-        x=z
-        print(i, "= ",z)
+    i=1
+    print(i,"= ", 0)
+    
+    i=2
+    if(x==0):
+        while(i<=12):
+            z=x+y
+            y=x
+            x=z
+            print(i, "= ",z)
+            i +=1
+    elif(y==0):
+        while(i<=12):
+            z=x+y
+            x=y
+            y=z
+            print(i, "= ",z)
+            i +=1
+        
 fibonaci(0,1)
     # 1+0=1
     # 0+1=1
